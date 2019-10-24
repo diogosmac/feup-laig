523711cba6be4ac87b92ab3bbf0eae51cb4359dd
EduRibeiro00 <eribeiro306@gmail.com>
2019-10-24 10:52:27 +0100

    Começo do projeto 2

---
+++ Projeto_2/LAIGDemoParser20192020v1.1/MyInterface.js
@@ -0,0 +1,79 @@
+/**
+* MyInterface class, creating a GUI interface.
+*/
+class MyInterface extends CGFinterface {
+    /**
+     * @constructor
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     * Initializes the interface.
+     * @param {CGFapplication} application
+     */
+    init(application) {
+        super.init(application);
+        // init GUI. For more information on the methods, check:
+        //  http://workshop.chromeexperiments.com/examples/gui
+
+        this.gui = new dat.GUI();
+
+        // this.gui.add(this.scene, 'camera').name('Camera');
+
+        this.initKeys();
+
+        return true;
+    }
+
+    addLightsFolder() {
+    
+        var lightsFolder = this.gui.addFolder('Lights');
+        var i = 0;
+
+        for(var key in this.scene.graph.lights) {
+            if(i >= 8)
+                break;
+            
+            lightsFolder.add(this.scene.graph.lights[key], '0').name(key);
+            i++;
+        }
+    }
+
+    addCamerasDropdown() {
+
+        // fill in the array
+        for(var key in this.scene.graph.views) {
+            this.scene.interfaceArrayViews[key] = key;
+        }
+
+        this.gui.add(this.scene, 'activeCameraID', this.scene.interfaceArrayViews).name('Selected camera').onChange(this.scene.changeCamera.bind(this.scene));
+    }
+
+    updateInterface() {
+        this.addCamerasDropdown();
+        this.addLightsFolder();
+    }
+
+    /**
+     * initKeys
+     */
+    initKeys() {
+        this.scene.gui=this;
+        this.processKeyboard=function(){};
+        this.activeKeys={};
+    }
+
+    processKeyDown(event) {
+        this.activeKeys[event.code]=true;
+    };
+
+    processKeyUp(event) {
+        this.activeKeys[event.code]=false;
+    };
+
+    isKeyPressed(keyCode) {
+        return this.activeKeys[keyCode] || false;
+    }
+}
---
+++ Projeto_2/LAIGDemoParser20192020v1.1/MyTorus.js
@@ -0,0 +1,90 @@
+/**
+ * MyTorus
+ * @constructor
+ * @param scene - Reference to MyScene object
+ * @param id - ID of the object
+ * @param inner - Inner radius of the torus
+ * @param outer - Outer radius of the torus
+ * @param loops - Number of desired loops
+ * @param slices - Number of desired slices
+ */
+class MyTorus extends CGFobject {
+
+    constructor(scene, id, inner, outer, slices, loops) {
+        super(scene);
+        this.id = id;
+        this.inner = inner;
+        this.outer = outer;
+        this.loops = loops;
+        this.slices = slices;
+
+        this.initBuffers();
+    }
+
+    initBuffers() {
+        this.vertices = [];
+        this.normals = [];
+        this.indices = [];
+        this.texCoords = [];
+
+        var centerX, centerY;
+        var vertexX, vertexY, vertexZ;
+        var normal = vec3.create(0, 0, 0);
+
+        for (var slice = 0; slice <= this.slices; slice++) {
+
+            for (var loop = 0; loop <= this.loops; loop++) {
+
+                var u = (loop / this.loops) * Math.PI * 2;
+                var v = (slice / this.slices) * Math.PI * 2;
+
+                vertexX = (this.outer + this.inner * Math.cos(v)) * Math.cos(u);
+                vertexY = (this.outer + this.inner * Math.cos(v)) * Math.sin(u);
+                vertexZ = this.inner * Math.sin(v);
+
+                this.vertices.push(vertexX, vertexY, vertexZ);
+
+                centerX = this.outer * Math.cos(u);
+                centerY = this.outer * Math.sin(u);
+                normal.x = vertexX - centerX;
+                normal.y = vertexY - centerY;
+                normal.z = vertexZ;
+    
+                vec3.normalize(normal, normal);
+
+                this.normals.push(normal.x, normal.y, normal.z);
+
+                // orientacao das coordenadas de textura sujeita a interpretacao
+                // this.texCoords.push(slice / this.slices, loop / this.loops);
+                this.texCoords.push(loop / this.loops, slice / this.slices);
+
+            }
+
+        }
+
+
+        for (var slice = 0; slice < this.slices; slice++) {
+            
+            for (var loop = 0; loop < this.loops; loop++) {
+
+                var vertex1 = ( this.loops + 1 ) * ( slice + 1 ) + loop;
+                var vertex2 = ( this.loops + 1 ) * slice + loop;
+                var vertex3 = ( this.loops + 1 ) * slice + ( loop + 1 );
+                var vertex4 = ( this.loops + 1 ) * ( slice + 1 ) + ( loop + 1 );
+
+                this.indices.push(vertex1, vertex2, vertex4);
+                this.indices.push(vertex2, vertex3, vertex4);
+
+            }
+
+        }
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+
+    display(ls, lt) {
+        super.display();
+    }
+
+}
