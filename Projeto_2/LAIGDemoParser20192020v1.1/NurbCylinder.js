91b6ee61649d75f2b9d2ece0f0f14b8e153877bb
Diogo Machado <diogo.smac@gmail.com>
2019-11-08 19:57:57 +0000

    Nurb cylinder fixed

--- Projeto_2/LAIGDemoParser20192020v1.1/NurbCylinder.js
+++ Projeto_2/LAIGDemoParser20192020v1.1/NurbCylinder.js
@@ -27,96 +27,47 @@ class NurbCylinder extends CGFobject {
      * Function that creates the 2 nurbs objects that the cylinder displays
      */
     initBuffers() {
-        // this.controlPointsA = [
-        //     // U = 0
-        //     [   // V = 0..4
-        //         [0, -this.base, 0, 1],
-        //         [-this.base, -this.base, 0, Math.sqrt(2)/2],
-        //         [-this.base, 0, 0, 1],
-        //         [-this.base, this.base, 0, Math.sqrt(2)/2],
-        //         [0, this.base, 0, 1]
-        //     ],
-        //     // U = 1
-        //     [   // V = 0..4
-        //         [0, -this.top, this.height, 1],
-        //         [-this.top, -this.top, this.height, Math.sqrt(2)/2],
-        //         [-this.top, 0, this.height, 1],
-        //         [-this.top, this.top, this.height, Math.sqrt(2)/2],
-        //         [0, this.top, this.height, 1]
-        //     ]
-        // ];
-        // var surfA = new CGFnurbsSurface(1, 4, this.controlPointsA);
-        // this.objA = new CGFnurbsObject(this.scene, this.stacks, Math.round(this.slices / 2), surfA);
-
-        // this.controlPointsB = [
-        //     // U = 0
-        //     [   // V = 0..4
-        //         [0, this.base, 0, 1],
-        //         [this.base, this.base, 0, Math.sqrt(2)/2],
-        //         [this.base, 0, 0, 1],
-        //         [this.base, -this.base, 0, Math.sqrt(2)/2],
-        //         [0, -this.base, 0, 1]
-        //     ],
-        //     // U = 1
-        //     [   // V = 0..4
-        //         [0, this.top, this.height, 1],
-        //         [this.top, this.top, this.height, Math.sqrt(2)/2],
-        //         [this.top, 0, this.height, 1],
-        //         [this.top, -this.top, this.height, Math.sqrt(2)/2],
-        //         [0, -this.top, this.height, 1]
-        //     ]
-        // ];
-        // var surfB = new CGFnurbsSurface(1, 4, this.controlPointsB);
-        // this.objB = new CGFnurbsObject(this.scene, this.stacks, Math.round(this.slices / 2), surfB);
-
-        this.controlPointsA = [
+        var controlPointsA = [
             // U = 0
-            [   // V = 0..5
+            [   // V = 0..3
                 [0, -this.base, 0, 1],
-                [-this.base, -this.base, 0, 1],
-                [-this.base, 0, 0, 1],
-                [-this.base, 0, 0, 1],
-                [-this.base, this.base, 0, 1],
+                [-this.base * 4/3, -this.base, 0, 1],
+                [-this.base * 4/3, this.base, 0, 1],
                 [0, this.base, 0, 1]
             ],
             // U = 1
-            [   // V = 0..5
+            [   // V = 0..3
                 [0, -this.top, this.height, 1],
-                [-this.top, -this.top, this.height, 1],
-                [-this.top, 0, this.height, 1],
-                [-this.top, 0, this.height, 1],
-                [-this.top, this.top, this.height, 1],
+                [-this.top * 4/3, -this.top, this.height, 1],
+                [-this.top * 4/3, this.top, this.height, 1],
                 [0, this.top, this.height, 1]
             ]
         ];
-        var surfA = new CGFnurbsSurface(1, 5, this.controlPointsA);
+        var surfA = new CGFnurbsSurface(1, 3, controlPointsA);
         this.objA = new CGFnurbsObject(this.scene, this.stacks, Math.round(this.slices / 2), surfA);
 
-        this.controlPointsB = [
+        var controlPointsB = [
             // U = 0
-            [   // V = 0..5
+            [   // V = 0..3
                 [0, this.base, 0, 1],
-                [this.base, this.base, 0, 1],
-                [this.base, 0, 0, 1],
-                [this.base, 0, 0, 1],
-                [this.base, -this.base, 0, 1],
+                [this.base * 4/3, this.base, 0, 1],
+                [this.base * 4/3, -this.base, 0, 1],
                 [0, -this.base, 0, 1]
             ],
             // U = 1
-            [   // V = 0..5
+            [   // V = 0..3
                 [0, this.top, this.height, 1],
-                [this.top, this.top, this.height, 1],
-                [this.top, 0 ,this.height, 1],
-                [this.top, 0 ,this.height, 1],
-                [this.top, -this.top, this.height, 1],
+                [this.top * 4/3, this.top, this.height, 1],
+                [this.top * 4/3, -this.top, this.height, 1],
                 [0, -this.top, this.height, 1]
             ]
         ];
-        var surfB = new CGFnurbsSurface(1, 5, this.controlPointsB);
+        var surfB = new CGFnurbsSurface(1, 3, controlPointsB);
         this.objB = new CGFnurbsObject(this.scene, this.stacks, Math.round(this.slices / 2), surfB);
     }
 
     display(ls, lt) {
+        console.log("AH");
         this.objA.display();
         this.objB.display();
     }
